923cb2e17241c3b797e072290c4b95804bad846f
gaetan.perez@icloud.com <gaetan.perez@icloud.com>
2024-03-27 19:59:27 +0100

    add backup mysql

--- mysql.py
+++ mysql.py
@@ -1,5 +1,8 @@
 import pymysql
 import subprocess
+import time
+from time import gmtime, strftime
+
 
 def backup_mysql():
     # Define database connection details
@@ -7,16 +10,16 @@ def backup_mysql():
     user = "gperez"
     password = "Letouille0101!!"
     database = "ce-prod"
-    backup_file = "backup_short.sql"
+    backup_file = "backup_short.dmp"
     port = 7347
-
+    db_file_name= "db_short_" + time.strftime("%Y%m%d%I%M") + ".dmp"
     # Establish connection with the MySQL database
     connection = pymysql.connect(host=host, user=user, password=password, database=database, port=port)
     
     # Execute the mysqldump command
     #command = f"mysqldump −h{host} −u{user} −-password='{password}' -P{port} {database} > {backup_file}"
-    command = f"mysqldump -u {user} --password='{password}' -h {host} -P {port} {database} --ignore-table=ce-prod.ce_login_attempts > db_short.sql"
-    #print (command)
+    command = f"mysqldump -u {user} --password='{password}' -h {host} -P {port} {database} --ignore-table=ce-prod.ce_login_attempts > /root/gp/backup/'{db_file_name}'"
+    print (command)
     subprocess.run(command, shell=True)
 
     # Close the database connection
